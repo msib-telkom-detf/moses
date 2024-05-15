eecc6a72e973627a6f929a12cb61e943a7d41c4f
alfath.sulistianto <alfathns1932@gmail.com>
2024-05-15 14:35:13 +0700

    update syntax sql

--- clickhouse/docker-entrypoint-initdb.d/initiation.sql
+++ clickhouse/docker-entrypoint-initdb.d/initiation.sql
@@ -50,40 +50,34 @@ INSERT INTO imdb.actors
 SELECT *
 FROM s3('https://datasets-documentation.s3.eu-west-3.amazonaws.com/imdb/imdb_ijs_actors.tsv.gz',
 'TSVWithNames')
-WHERE NOT EXISTS (SELECT 1 FROM system.tables WHERE database = 'imdb' AND name = 'actors');
 
 -- Insert data if table imdb.directors does not exist
 INSERT INTO imdb.directors
 SELECT *
 FROM s3('https://datasets-documentation.s3.eu-west-3.amazonaws.com/imdb/imdb_ijs_directors.tsv.gz',
 'TSVWithNames')
-WHERE NOT EXISTS (SELECT 1 FROM system.tables WHERE database = 'imdb' AND name = 'directors');
 
 -- Insert data if table imdb.genres does not exist
 INSERT INTO imdb.genres
 SELECT *
 FROM s3('https://datasets-documentation.s3.eu-west-3.amazonaws.com/imdb/imdb_ijs_movies_genres.tsv.gz',
 'TSVWithNames')
-WHERE NOT EXISTS (SELECT 1 FROM system.tables WHERE database = 'imdb' AND name = 'genres');
 
 -- Insert data if table imdb.movie_directors does not exist
 INSERT INTO imdb.movie_directors
 SELECT *
 FROM s3('https://datasets-documentation.s3.eu-west-3.amazonaws.com/imdb/imdb_ijs_movies_directors.tsv.gz',
         'TSVWithNames')
-WHERE NOT EXISTS (SELECT 1 FROM system.tables WHERE database = 'imdb' AND name = 'movie_directors');
 
 -- Insert data if table imdb.movies does not exist
 INSERT INTO imdb.movies
 SELECT *
 FROM s3('https://datasets-documentation.s3.eu-west-3.amazonaws.com/imdb/imdb_ijs_movies.tsv.gz',
 'TSVWithNames')
-WHERE NOT EXISTS (SELECT 1 FROM system.tables WHERE database = 'imdb' AND name = 'movies');
 
 -- Insert data if table imdb.roles does not exist
 INSERT INTO imdb.roles(actor_id, movie_id, role)
 SELECT actor_id, movie_id, role
 FROM s3('https://datasets-documentation.s3.eu-west-3.amazonaws.com/imdb/imdb_ijs_roles.tsv.gz',
 'TSVWithNames')
-WHERE NOT EXISTS (SELECT 1 FROM system.tables WHERE database = 'imdb' AND name = 'roles');
 
