06ff62f4be1c7a7eb950827db9f8777fdad34616
alfath.sulistianto <alfathns1932@gmail.com>
2024-05-28 11:23:13 +0700

    add task ingest data from airbyte

--- airflow/dags/operators.py
+++ airflow/dags/operators.py
@@ -1,10 +1,7 @@
 import pendulum
 from airflow.models.dag import DAG
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
-# from airflow_clickhouse_plugin.operators.clickhouse import ClickHouseOperator
 
 with DAG(
     dag_id="test-dbt",
@@ -13,38 +10,25 @@ with DAG(
     catchup=False,
     tags=["test-dbt"],
 ):
-    # sync_operator = AirbyteTriggerSyncOperator(
-    #     task_id='sync_operator',
-    #     airbyte_conn_id='airbyte_connector',
-    #     connection_id='722049c4-5e54-4388-9d26-3002f42c8af0',
-    #     asynchronous=False,
-    #     timeout=3600,
-    #     wait_seconds=3
-    # )
+    
+    ingest_data = AirbyteTriggerSyncOperator(
+        task_id='sync_operator',
+        airbyte_conn_id='airbyte_connector',
+        connection_id='722049c4-5e54-4388-9d26-3002f42c8af0',
+        asynchronous=False,
+        timeout=3600,
+        wait_seconds=3
+    )
 
     dbt_command = """
     cd /usr/src/dbt/test_social_media # Activate dbt virtual environment if needed
     dbt run # Run dbt command
     """
 
-    run_dbt = SSHOperator(
+    cast_json_data = SSHOperator(
         task_id='run_dbt_command',
-        ssh_conn_id='dbt_conn',  # SSH connection ID in Airflow
+        ssh_conn_id='dbt_conn',
         command=dbt_command,
     )
 
-    run_dbt
-
-    # sync_operator >> casting_data
-
-    # task_dbt_run = BashOperator(
-    #     task_id="dbt_run",
-    #     bash_command="dbt run --profiles-dir /opt/airflow/dbt/imdb --project-dir /opt/airflow/dbt/imdb",
-    # )
-
-    # # task_dbt_test = BashOperator(
-    # #     task_id="dbt_test",
-    # #     bash_command="dbt test --profiles-dir /opt/airflow/dbt-profiles --project-dir /tmp/pizza_shop",
-    # # )
-
-    # task_dbt_run
+    ingest_data >> cast_json_data
