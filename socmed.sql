4c1099cb5b90e01e9438c394286ed88751571259
eciemriyeni3 <127744462+eciemriyeni3@users.noreply.github.com>
2024-06-13 11:11:20 +0700

    Delete socmed.sql

--- socmed.sql
+++
@@ -1,6 +0,0 @@
---Menghitung skala kesehatan mental rata-rata berdasarkan gender--
-SELECT ud.gender, AVG(mhf.scale_feel_depressed) AS avg_depressed
-FROM user_dim ud
-JOIN mental_health_fact mhf ON ud.user_id = mhf.user_id
-GROUP BY ud.gender;
-
