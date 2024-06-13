f866eaf5aa47a4470bf9f7da6d0ec9603188073d
eci.emriyeni <eciemriyeni3@gmail.com>
2024-06-13 13:46:17 +0700

    edit sql mental_health_fact.sql

--- dbt/social_media_mental_health/models/dw_social_media_mental_health/mental_health_fact.sql
+++ dbt/social_media_mental_health/models/dw_social_media_mental_health/mental_health_fact.sql
@@ -2,17 +2,12 @@
 
 with mental_health_fact as (
     SELECT
-        m.user_id,
-        m.social_media_id,
-        m.scale_worries,
-        m.scale_difficult_to_concentrate,
-        m.scale_feel_depressed,
-        m.scale_often_face_issues_regarding_sleep    
+        ROW_NUMBER() OVER () AS user_id,
+        ROW_NUMBER() OVER () AS social_media_id,
+        scale_difficult_to_concentrate,
+        scale_feel_depressed,
+        scale_often_face_issues_regarding_sleep    
     FROM
-        {{ source('structured_data', 'social_media_mental_health') }} AS m
-    JOIN
-        user_dimension u ON m.user_id = u.user_id
-    JOIN
-        social_media_dimension s ON m.social_media_id = s.social_media_id;
+        {{ source('structured_data', 'social_media_mental_health') }} 
 )
 SELECT * FROM mental_health_fact 
