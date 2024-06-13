3550e94167ccd0884f21d4ee6ebd267e83de613d
eci.emriyeni <eciemriyeni3@gmail.com>
2024-06-13 14:05:02 +0700

    edit mental_health_fact.sql

--- dbt/social_media_mental_health/models/dw_social_media_mental_health/mental_health_fact.sql
+++ dbt/social_media_mental_health/models/dw_social_media_mental_health/mental_health_fact.sql
@@ -6,7 +6,8 @@ with mental_health_fact as (
         ROW_NUMBER() OVER () AS social_media_id,
         scale_difficult_to_concentrate,
         scale_feel_depressed,
-        scale_often_face_issues_regarding_sleep    
+        scale_often_face_issues_regarding_sleep,
+        scale_worries   
     FROM
         {{ source('structured_data', 'social_media_mental_health') }} 
 )
