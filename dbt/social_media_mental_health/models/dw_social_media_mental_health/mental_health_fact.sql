578019973fe41ddd97d8d90cccde00ad89c5b77b
eciemriyeni3 <127744462+eciemriyeni3@users.noreply.github.com>
2024-06-13 13:32:17 +0700

    update query mental_health_fact.sql

--- dbt/social_media_mental_health/models/dw_social_media_mental_health/mental_health_fact.sql
+++ dbt/social_media_mental_health/models/dw_social_media_mental_health/mental_health_fact.sql
@@ -2,8 +2,8 @@
 
 with mental_health_fact as (
     SELECT
-        u.user_id,
-        s.social_media_id,
+        m.user_id,
+        m.social_media_id,
         m.scale_worries,
         m.scale_difficult_to_concentrate,
         m.scale_feel_depressed,
@@ -11,14 +11,8 @@ with mental_health_fact as (
     FROM
         {{ source('structured_data', 'social_media_mental_health') }} AS m
     JOIN
-        user_dimension u ON m.age = u.age
-        AND m.gender = u.gender
-        AND m.relationship_status = u.relationship_status
-        AND m.occupation_status = u.occupation_status
-        AND m.affiliated_organization = u.affiliated_organization
+        user_dimension u ON m.user_id = u.user_id
     JOIN
-        social_media_dimension s ON m.uses_social_media = s.uses_social_media
-        AND m.commonly_used_social_media_platforms = s.commonly_used_social_media_platforms
-        AND m.average_daily_use_of_social_media = s.average_daily_use_of_social_media;
+        social_media_dimension s ON m.social_media_id = s.social_media_id;
 )
 SELECT * FROM mental_health_fact 
