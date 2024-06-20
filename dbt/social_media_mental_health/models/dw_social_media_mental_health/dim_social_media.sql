3ac396a66d75a39015fb4549f594e3beb271c151
eci.emriyeni <eciemriyeni3@gmail.com>
2024-06-20 12:54:19 +0700

    add filter extract_time

--- dbt/social_media_mental_health/models/dw_social_media_mental_health/dim_social_media.sql
+++ dbt/social_media_mental_health/models/dw_social_media_mental_health/dim_social_media.sql
@@ -4,8 +4,9 @@ with dim_social_media as (
     SELECT
         ROW_NUMBER() OVER () AS social_media_id,
         uses_social_media,
-        commonly_used_social_media_platforms 
+        commonly_used_social_media_platforms
     FROM
         {{ source('structured_data', 'social_media_mental_health') }}
+    WHERE DATE(extract_time) = SUBDATE(CURDATE(), INTERVAL 1 DAY)
 )
 SELECT * FROM dim_social_media
